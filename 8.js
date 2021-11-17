62e72bfed5fc955a542fb339e65ef65c459b19e8
JYSC <89956471+JYSC@users.noreply.github.com>
2021-11-16 22:23:03 -0700

    Create 8.js

---
+++ 8.js
@@ -0,0 +1,18 @@
+console.log("Introduce 'S' o 'N' ");
+var char;
+process.stdin.on('data', function(data){
+    char = data.toString().trim();
+    if(char == 'S' || char == 's')
+    {
+        console.log("Ingresaste S");
+    }
+    else if(char == 'N' || char == 'n')
+    {
+        console.log("Ingresaste N");
+    }
+    else
+    {
+        console.log("Vuelve a intentarlo.");
+    }
+    process.exit();
+});
